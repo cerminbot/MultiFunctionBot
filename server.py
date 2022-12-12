f17a403f82a7f94876a28b88859d28228a38dd31
yasirarism <55983182+yasirarism@users.noreply.github.com>
2022-12-12 10:16:45 +0700

    Update server.py
    Signed-off-by: yasirarism <55983182+yasirarism@users.noreply.github.com>

--- server.py
+++ server.py
@@ -1,13 +1,22 @@
 import os
 from flask import Flask
-from flask_restful import Resource, Api
 
 app = Flask(__name__)
-api = Api(app)
 
-class Greeting (Resource):
-    def get(self):
-        return "MultiFunction Bot is Up & Running!"
+@app.route('/')
+def root():
+    return "MultiFunction Bot is Up & Running!"
 
-api.add_resource(Greeting, '/')
+@app.route('/pahe')
+def pahe():
+    if request.args.get('q'):
+        judul = request.args.get('q')
+        return judul
+    else:
+        return {
+            'success': False,
+            'msg': 'Isi parameter query, ex: https://yasirapi.eu.org/yt-search?q=scraping%20tutorial',
+            'info': 'Join telegram channel @YasirPediaChannel for updates.',
+        }
+
 app.run(host="0.0.0.0", port=os.environ.get("PORT", 8080))
