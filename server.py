d7f829d6af53696c622326a46dd3614bdbaf7e0f
yasirarism <55983182+yasirarism@users.noreply.github.com>
2022-12-12 10:30:04 +0700

    Update server.py
    Signed-off-by: yasirarism <55983182+yasirarism@users.noreply.github.com>

--- server.py
+++ server.py
@@ -1,8 +1,34 @@
 import os
-import requests
+from bs4 import BeautifulSoup
+import chromedriver_autoinstaller
+from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.support.ui import WebDriverWait
 from flask import Flask, request
 
 app = Flask(__name__)
+
+def pahe(title: str) -> str:
+    chromedriver_autoinstaller.install()
+
+    AGREE_BUTTON = "//*[contains(text(), 'AGREE')]"
+    LINK_TYPE = ["//*[contains(text(), 'GD')]"]
+    GENERATE = "#generater > img"
+    SHOW_LINK = "showlink"
+    CONTINUE = "Continue"
+
+    os.chmod("/usr/src/app/chromedriver", 755)
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.binary_location = "/usr/bin/google-chrome"
+    chrome_options.add_argument("--start-maximized")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--headless")
+    wd = webdriver.Chrome("/usr/src/app/chromedriver", chrome_options=chrome_options)
+    wd.get(f"https://pahe.li/?s={title}")
+    retun wd.page_source
 
 @app.route('/')
 def root():
@@ -12,7 +38,8 @@ def root():
 def pahe():
     if request.args.get('q'):
         judul = request.args.get('q')
-        return judul
+        parse = BeautifulSoup(pahe(judul), "lxml")
+        return parse
     else:
         return {
             'success': False,
